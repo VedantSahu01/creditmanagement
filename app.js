617459eeed04d20b4714a95a07ecffed2eef94ff
Vedant Sahu <vedantsahu007@gmail.com>
2020-08-30 02:19:14 +0530

    Change ports

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 
