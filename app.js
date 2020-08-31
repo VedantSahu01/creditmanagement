3413013699653aba11e0abee4b8b0c2ce3279b72
VedantSahu01 <64314087+VedantSahu01@users.noreply.github.com>
2020-08-31 18:42:56 +0530

    Hide Data

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin-Vedant:VeSa%232001@cluster0.zy2bu.mongodb.net/creditManagement', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://admin-USERNAME:PASSWORD1@cluster0.zy2bu.mongodb.net/creditManagement', {useNewUrlParser: true});
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
