e544ae5418ad96b70647d82f8f10bb2f51078df7
VedantSahu01 <64314087+VedantSahu01@users.noreply.github.com>
2020-08-31 18:39:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,13 +19,6 @@ const transferSchema = new mongoose.Schema({
 var User = mongoose.model('User', userSchema);
 var Transfer = mongoose.model('Transfer', transferSchema);
 
-// User.insertMany([{name:"Arjun" , email:"arjun01@gmail.com" , credit:50 },{name:"Riya" , email:"sharma.riya@gmail.com" , credit:405 },{name:"Kabir" , email:"kabir5925@mail.com" , credit:300 },
-// {name:"Diya" , email:"diya@mail.com" , credit:250 },{name:"Aditya" , email:"adityagupta70@gmail.com" , credit:69 },{name:"Siddarth" , email:"siddshukla@mail.com" , credit:200 },
-// {name:"Yash" , email:"yashsahu007@gmail.com" , credit: 420 },{name:"Naina" , email:"naina1995@mail.com" , credit:20 },{name:"Jyoti" , email:"jyotilbs@gmail.com" , credit:350 },
-// {name:"Aditi" , email:"aditi@mail.com" , credit:40 }], function(error, docs) {
-//   console.log(error);
-// });
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
